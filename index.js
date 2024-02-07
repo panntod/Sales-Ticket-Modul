2185d243eb05d63e7d5245ba32bea1faf59dc295
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-07 12:46:56 +0700

    feat: added endpoint to connect user route

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const mysql = require("mysql2");
 const dotenv = require("dotenv").config();
 const cors = require(`cors`);
-
 const app = express();
-const port = process.env.APP_PORT || 5000;
+const port = process.env.APP_PORT;
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.static(__dirname));
+
+const userRouter = require("./routes/user-route");
+
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server listen to ${port}`);
