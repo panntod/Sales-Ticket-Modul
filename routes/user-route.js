f7dbc315451b96a4589763fc84048569a5d6230c
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-07 12:46:07 +0700

    feat: added route to user controller

---
+++ routes/user-route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const { validateUser } = require("../middleware/user-premision");
+const userController = require("../controller/user-controller");
+const { authorize } = require("../middleware/auth");
+const { IsUser, IsAdmin } = require("../middleware/role-validation");
+
+app.get("/getAll", userController.getAllUser);
+app.get("/:id", userController.getUserById);
+app.post("/", userController.addUser);
+app.put("/:id", userController.updateUserById);
+app.patch("/:id", userController.changePassword);
+app.delete("/:id", IsAdmin, userController.deleteUser);
+
+module.exports = app;
