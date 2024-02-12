97f1306358e65ea2009297d6db068fb3f2d54f18
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-12 08:40:21 +0700

    refactor: change app into router

--- routes/event-route.js
+++ routes/event-route.js
@@ -1,15 +1,14 @@
 const express = require(`express`)
-const app = express()
-app.use(express.json())
+const router = express.Router();
 
 const eventController = require('../controller/event-controller')
 const { authorize } = require('../middleware/auth')
 const { IsUser, IsAdmin } = require('../middleware/role-validation')
 
-app.get("/", authorize, IsAdmin, eventController.getAllEvent)
-app.get("/:key", authorize, IsUser, eventController.findEvent)
-app.post("/", authorize, IsAdmin, eventController.addEvent)
-app.put("/:id", authorize, IsAdmin, eventController.updateEvent)
-app.delete("/:id", authorize, IsAdmin, eventController.deleteEvent)
+router.get("/", authorize, IsAdmin, eventController.getAllEvent)
+router.get("/:key", authorize, IsUser, eventController.findEvent)
+router.post("/", authorize, IsAdmin, eventController.addEvent)
+router.put("/:id", authorize, IsAdmin, eventController.updateEvent)
+router.delete("/:id", authorize, IsAdmin, eventController.deleteEvent)
 
-module.exports = app
+module.exports = router
